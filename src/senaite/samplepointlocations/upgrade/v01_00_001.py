944eccee04297f9cdfff13a47b64fa820bfed032
Lunga Baliwe <lunga001@gmail.com>
2022-08-17 12:49:44 +0200

    Fix upgrade script

--- src/senaite/samplepointlocations/upgrade/v01_00_001.py
+++ src/senaite/samplepointlocations/upgrade/v01_00_001.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from bika.lims import api
 from senaite.crms import PRODUCT_NAME
-from senaite.crms import PROFILE_ID
 from senaite.crms import logger
 
 from senaite.core.upgrade import upgradestep
-from senaite.core.upgrade.utils import UpgradeUtils
 
 version = "1.0.1"
 
@@ -13,27 +12,12 @@ version = "1.0.1"
 @upgradestep(PRODUCT_NAME, version)
 def upgrade(tool):
     portal = tool.aq_inner.aq_parent
-    setup = portal.portal_setup
-    ut = UpgradeUtils(portal)
-    ver_from = ut.getInstalledVersion(PRODUCT_NAME)
-
-    if ut.isOlderVersion(PRODUCT_NAME, version):
-        logger.info(
-            "Skipping upgrade of {0}: {1} > {2}".format(PRODUCT_NAME, ver_from, version)
-        )
-        return True
-
-    logger.info("Upgrading {0}: {1} -> {2}".format(PRODUCT_NAME, ver_from, version))
-
-    # -------- ADD YOUR STUFF BELOW --------
-
-    setup.runImportStepFromProfile(PROFILE_ID, "plone.app.registry")
-
+    change_action_title(portal)
     logger.info("{0} upgraded to version {1}".format(PRODUCT_NAME, version))
     return True
 
 
-def add_location_to_client(portal):
+def change_action_title(portal):
     pt = api.get_tool("portal_types", context=portal)
     fti = pt.get("Client")
     actions = fti.listActions()
