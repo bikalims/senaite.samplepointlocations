4da31a53247f01a330ce43678d3d2b31fcab34a5
luthando5 <104898641+luthando5@users.noreply.github.com>
2022-08-30 10:14:23 +0200

    Upgrade to 1.0.3 (#1)
    Co-authored-by: Arneldo <ta.arneldo@gmail.com>

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="senaite.samplepointlocations",
-    version="1.0.2",
+    version="1.0.3",
     description="Two tier Sample Points",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
