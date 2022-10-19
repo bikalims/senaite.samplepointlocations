9a18d0f3a369314d9887c5ce8e7258e9c1825325
Mike Metcalfe <mike@webtide.co.za>
2022-10-19 14:22:54 +0200

    Removed python3 references in setup

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setup(
         "Framework :: Plone :: 5.2",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.7",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
@@ -47,7 +46,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires="==2.7, >=3.6",
+    python_requires="==2.7",
     install_requires=[
         "setuptools",
         # -*- Extra requirements: -*-
