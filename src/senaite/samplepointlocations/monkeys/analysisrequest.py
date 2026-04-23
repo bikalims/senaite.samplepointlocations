d18f3d6ac0745dc0522c20c1ed539a8f02316459
Lunga Baliwe <lunga001@gmail.com>
2026-04-23 10:08:09 +0200

    LIMS-1904

--- src/senaite/samplepointlocations/monkeys/analysisrequest.py
+++ src/senaite/samplepointlocations/monkeys/analysisrequest.py
@@ -120,7 +120,7 @@ def get_samplepointlocation_info(obj, info, client_uid, client_metadata={}):
 
     # catalog queries for UI field filtering
 
-    # update client_metadata info
+    import pdb; pdb.set_trace()
     if client_metadata:
         location_uid = obj.UID()
         filter_queries = client_metadata["filter_queries"]
@@ -168,6 +168,57 @@ def get_samplepoint_info(obj, info, client_uid):
     return info
 
 
+def get_client_queries(self, obj, record=None):
+    """Returns the filter queries to be applied to other fields based on
+    both the Client object and record
+    """
+    # UID of the client
+    uid = api.get_uid(obj)
+
+    # catalog queries for UI field filtering
+    queries = {
+        "Contact": {
+            "getParentUID": [uid, ""]
+        },
+        "CCContact": {
+            "getParentUID": [uid, ""]
+        },
+        "SamplePoint": {
+            "getClientUID": [uid, ""],
+        },
+        "Template": {
+            "getClientUID": [uid, ""],
+        },
+        "Profiles": {
+            "getClientUID": [uid, ""],
+        },
+        "Specification": {
+            "getClientUID": [uid, ""],
+        },
+        "Sample": {
+            "getClientUID": [uid],
+        },
+        "Batch": {
+            "getClientUID": [uid, ""],
+        },
+        "PrimaryAnalysisRequest": {
+            "getClientUID": [uid, ""],
+        },
+        "SamplePointLocation": {
+            "getClientUID": [uid, ""],
+            },
+    }
+
+    # additional filtering by sample type
+    record = record if record else {}
+    sample_type_uid = record.get("SampleType")
+    if api.is_uid(sample_type_uid):
+        st_queries = self.get_sampletype_queries(sample_type_uid, record)
+        queries.update(st_queries)
+
+    return queries
+
+
 def get_objects_info(self, record, key, client_metadata):
     """
     Returns a list with the metadata for the objects the field with
@@ -209,6 +260,21 @@ def get_client_info(self, obj):
 
     return info
 
+
+def get_samplepointlocation_queries(self, obj, record=None):
+    """Returns the filter queries to apply to other fields based on both
+    the SamplePoint object and record
+    """
+    uid = api.get_uid(obj)
+    queries = {
+        # Display Sample Points that have this sample type assigned plus
+        # those that do not have a sample type assigned
+        "SamplePoint": {
+            "getParentUID": [uid, ""],
+        },
+    }
+
+    return queries
 
 def ajax_get_flush_settings(self):
     """Returns the settings for fields flush"""
